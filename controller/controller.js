81fc745c05f99b55e0bc3fd63e7e0528da98b534
suddasgu <suddasgu@in.ibm.com>
2017-12-07 13:43:23 +0530

    Add files via upload

---
+++ controller/controller.js
@@ -0,0 +1,27 @@
+var services = require("../service/node_cloudant_service");
+var bodyParser  = require("body-parser");
+
+
+
+// all service Request comes here
+module.exports = function(app) {
+
+	app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.post('/api/addCustomerDetails', function(req , res){
+           var details = req.body;
+            services.addUserProfile(res, details , function (found) {
+                res.json(found);    
+                res.end();
+            });            
+    });
+	
+	app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.get('/api/getCustomerDetails', function(req , res){
+            services.fetchUserProfiles(res, function (found) {
+                res.json(found);    
+                res.end();
+            });            
+    });
+}
