4dd14614085e2549bfa59b6e4e1156fef1f3b227
Angshuman Dey <angsdey2@in.ibm.com>
2017-12-09 14:43:45 +0530

    Add search UI added
    Add search UI added

--- app.js
+++ app.js
@@ -17,6 +17,26 @@ var app = express();
 
 require('./controller/controller.js')(app);
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
